a09a2c3eea44c09118f3c051045aa401e1f690d8
wen-05 <aiot63324@gmail.com>
2025-03-03 20:52:43 +0800

    refactor: handleErrorAsync 改寫

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,176 +11,153 @@ const generateJWT = require('../utils/generateJWT')
 
 const saltRounds = 10
 
-const signup = async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body
+const signup = handleErrorAsync(async (req, res, next) => {
+  const { name, email, password } = req.body
 
-    if (isUndefined(name) || isNotValidString(name) || isUndefined(email) || isNotValidString(email) || isUndefined(password) || isNotValidString(password)) {
+  if (isUndefined(name) || isNotValidString(name) || isUndefined(email) || isNotValidString(email) || isUndefined(password) || isNotValidString(password)) {
 
-      logger.warn('欄位未填寫正確')
-      next(appError(400, '欄位未填寫正確'))
-      return
-    }
-
-    if (!isValidPassword(password)) {
-      logger.warn('建立使用者錯誤: 密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字')
-      next(appError(400, '密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字'))
-      return
-    }
-
-    const userRepository = dataSource.getRepository('User')
-
-    // 檢查 email 是否已存在
-    const existingUser = await userRepository.findOneBy({ email })
-
-    if (existingUser) {
-      logger.warn('建立使用者錯誤: Email 已被使用')
-      next(appError(409, 'Email 已被使用'))
-      return
-    }
-
-    // 建立新使用者
-    const hashPassword = await bcrypt.hash(password, saltRounds)
-    const newUser = userRepository.create({
-      name,
-      email,
-      role: 'USER',
-      password: hashPassword
-    })
-
-    const savedUser = await userRepository.save(newUser)
-    logger.info('新建立的使用者ID:', savedUser.id)
-
-    handleSuccess(res, 201, {
-      user: {
-        id: savedUser.id,
-        name: savedUser.name
-      }
-    })
-  } catch (error) {
-    logger.error('建立使用者錯誤:', error)
-    next(error)
+    logger.warn('欄位未填寫正確')
+    next(appError(400, '欄位未填寫正確'))
+    return
   }
-}
 
-const login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body
-
-    if (isUndefined(email) || isNotValidString(email) || isUndefined(password) || isNotValidString(password)) {
-      logger.warn('欄位未填寫正確')
-      next(appError(400, '欄位未填寫正確'))
-      return
-    }
-
-
-    const userRepository = dataSource.getRepository('User')
-    const existingUser = await userRepository.findOne({
-      select: ['id', 'name', 'password'],
-      where: { email }
-    })
-
-    if (!existingUser) {
-      next(appError(400, '使用者不存在'))
-      return
-    }
-
-    logger.info(`使用者資料: ${JSON.stringify(existingUser)}`)
-
-    if (!isValidPassword(password)) {
-      logger.warn('密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字')
-      next(appError(400, '密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字'))
-      return
-    }
-
-    const isMatch = await bcrypt.compare(password, existingUser.password)
-    if (!isMatch) {
-      next(appError(400, '密碼輸入錯誤'))
-      return
-    }
-
-    const token = await generateJWT({
-      id: existingUser.id
-    }, config.get('secret.jwtSecret'), {
-      expiresIn: `${config.get('secret.jwtExpiresDay')}`
-    })
-
-    handleSuccess(res, 201, {
-      token,
-      user: {
-        name: existingUser.name
-      }
-    })
-
-  } catch (error) {
-    logger.error('登入錯誤:', error)
-    next(error)
+  if (!isValidPassword(password)) {
+    logger.warn('建立使用者錯誤: 密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字')
+    next(appError(400, '密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字'))
+    return
   }
-}
 
-const getProfile = async (req, res, next) => {
-  try {
-    const { id } = req.user
+  const userRepository = dataSource.getRepository('User')
 
-    const userRepository = dataSource.getRepository('User')
-    const user = await userRepository.findOne({
-      select: ['name', 'email'],
-      where: { id }
-    })
+  // 檢查 email 是否已存在
+  const existingUser = await userRepository.findOneBy({ email })
 
-    handleSuccess(res, 200, user)
-
-  } catch (error) {
-    logger.error('取得使用者資料錯誤:', error)
-    next(error)
+  if (existingUser) {
+    logger.warn('建立使用者錯誤: Email 已被使用')
+    next(appError(409, 'Email 已被使用'))
+    return
   }
-}
 
-const updateProfile = async (req, res, next) => {
-  try {
-    const { id } = req.user
-    const { name } = req.body
+  // 建立新使用者
+  const hashPassword = await bcrypt.hash(password, saltRounds)
+  const newUser = userRepository.create({
+    name,
+    email,
+    role: 'USER',
+    password: hashPassword
+  })
 
-    if (isUndefined(name) || isNotValidString(name)) {
-      logger.warn('欄位未填寫正確')
-      next(appError(400, '欄位未填寫正確'))
-      return
+  const savedUser = await userRepository.save(newUser)
+  logger.info('新建立的使用者ID:', savedUser.id)
+
+  handleSuccess(res, 201, {
+    user: {
+      id: savedUser.id,
+      name: savedUser.name
     }
+  })
+}, '建立使用者錯誤:')
 
-    const userRepository = dataSource.getRepository('User')
-    const user = await userRepository.findOne({
-      select: ['name'],
-      where: { id }
-    })
+const login = handleErrorAsync(async (req, res, next) => {
+  const { email, password } = req.body
 
-    if (user.name === name) {
-      next(appError(400, '使用者名稱未變更'))
-      return
-    }
-
-    const updatedResult = await userRepository.update({
-      id,
-      name: user.name
-    }, {
-      name
-    })
-
-    if (updatedResult.affected === 0) {
-      next(appError(400, '更新使用者資料失敗'))
-      return
-    }
-
-    const result = await userRepository.findOne({
-      select: ['name'],
-      where: { id }
-    })
-
-    handleSuccess(res, 200, { user: result })
-
-  } catch (error) {
-    logger.error('取得使用者資料錯誤:', error)
-    next(error)
+  if (isUndefined(email) || isNotValidString(email) || isUndefined(password) || isNotValidString(password)) {
+    logger.warn('欄位未填寫正確')
+    next(appError(400, '欄位未填寫正確'))
+    return
   }
-}
+
+
+  const userRepository = dataSource.getRepository('User')
+  const existingUser = await userRepository.findOne({
+    select: ['id', 'name', 'password'],
+    where: { email }
+  })
+
+  if (!existingUser) {
+    next(appError(400, '使用者不存在'))
+    return
+  }
+
+  logger.info(`使用者資料: ${JSON.stringify(existingUser)}`)
+
+  if (!isValidPassword(password)) {
+    logger.warn('密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字')
+    next(appError(400, '密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字'))
+    return
+  }
+
+  const isMatch = await bcrypt.compare(password, existingUser.password)
+  if (!isMatch) {
+    next(appError(400, '密碼輸入錯誤'))
+    return
+  }
+
+  const token = await generateJWT({
+    id: existingUser.id
+  }, config.get('secret.jwtSecret'), {
+    expiresIn: `${config.get('secret.jwtExpiresDay')}`
+  })
+
+  handleSuccess(res, 201, {
+    token,
+    user: {
+      name: existingUser.name
+    }
+  })
+}, '登入錯誤:')
+
+const getProfile = handleErrorAsync(async (req, res, next) => {
+  const { id } = req.user
+
+  const userRepository = dataSource.getRepository('User')
+  const user = await userRepository.findOne({
+    select: ['name', 'email'],
+    where: { id }
+  })
+
+  handleSuccess(res, 200, user)
+}, '取得使用者資料錯誤:')
+
+const updateProfile = handleErrorAsync(async (req, res, next) => {
+  const { id } = req.user
+  const { name } = req.body
+
+  if (isUndefined(name) || isNotValidString(name)) {
+    logger.warn('欄位未填寫正確')
+    next(appError(400, '欄位未填寫正確'))
+    return
+  }
+
+  const userRepository = dataSource.getRepository('User')
+  const user = await userRepository.findOne({
+    select: ['name'],
+    where: { id }
+  })
+
+  if (user.name === name) {
+    next(appError(400, '使用者名稱未變更'))
+    return
+  }
+
+  const updatedResult = await userRepository.update({
+    id,
+    name: user.name
+  }, {
+    name
+  })
+
+  if (updatedResult.affected === 0) {
+    next(appError(400, '更新使用者資料失敗'))
+    return
+  }
+
+  const result = await userRepository.findOne({
+    select: ['name'],
+    where: { id }
+  })
+
+  handleSuccess(res, 200, { user: result })
+}, '更新使用者資料錯誤:')
 
 module.exports = { signup, login, getProfile, updateProfile }
--- utils/handleErrorAsync.js
+++ utils/handleErrorAsync.js
@@ -2,12 +2,12 @@
 const logger = require('../utils/logger')('handleErrorAsync');
 
 // 參數 func 傳入 async 函式
-const handleErrorAsync = (fn) => {
+const handleErrorAsync = (fn, loggerErrorMessage = null) => {
   // 回傳 middleware 格式的新函式 
   return (req, res, next) => {
     // 執行傳入的 async 函式，使用 catch 統一捕捉
     fn(req, res, next).catch((error) => {
-      logger.error(error)
+      logger.error(loggerErrorMessage || error)
       next(error)
     });
   };
